76386f6d0c3d909efeed63ad4fd580847ffcada3
bueskyd <alexander@berg.dk>
2023-09-24 16:17:00 +0200

    Plugin

---
+++ src/PluginTest.hs
@@ -0,0 +1,12 @@
+module PluginTest (plugin) where
+import GhcPlugins
+
+plugin :: Plugin
+plugin = defaultPlugin {
+    installCoreToDos = install
+}
+
+install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
+install _ todo = do
+    putMsgS "Hello!"
+    return todo
